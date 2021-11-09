7d526688e063dc8469b9b97430b98844327a475a
Benoît Dequick <benoit.dequick@modernisation.gouv.fr>
2021-11-09 17:29:32 +0100

    add pnu page

---
+++ src/pages/PortailNumeriqueUnique.vue
@@ -0,0 +1,164 @@
+<template>
+  <Layout class="digital-accessibility-page">
+
+    <nav role="navigation" class="fr-breadcrumb" aria-label="vous êtes ici :">
+      <ol class="fr-breadcrumb__list">
+        <li>
+          <g-link to="/" class="fr-breadcrumb__link">Accueil</g-link>
+        </li>
+        <li>
+          <span aria-current="page">Portail numérique unique</span>
+        </li>
+      </ol>
+    </nav>
+
+    <div class="cover">
+      <div class="cover__container">
+        <h1 class="text-highlight"><Accessibilite class="h1__icon" focusable="false" aria-hidden="true"/>Portail numérique unique - Procédures</h1>
+        <p class="cover__subtitle">Les informations sur cette page ne concernent que le Pilier II du Portail numérique unique (PNU), excluant le Pilier I (pages d'information) et le Pilier III (service d'assistance).</p>
+      </div>
+    </div>
+
+    <div class="content">
+      <h2>Présentation</h2>
+      <h3>Ce que dit le règlement</h3>
+      <p>Le portail donne accès [à] des informations sur les procédures en ligne et hors ligne ainsi que des liens vers des procédures en ligne [...] pour permettre aux utilisateurs d’exercer les droits et de se conformer aux obligations et aux règles dans le domaine du marché intérieur. </p>
+      <p>On entend par «utilisateur»: un citoyen de l’Union, une personne physique résidant dans un État membre ou une personne morale ayant son siège statutaire dans un État membre [...]. </p>
+      <p>Les procédures concernées sont celles listées dans l'[Annexe II](https://eur-lex.europa.eu/legal-content/FR/TXT/HTML/?uri=CELEX:32018R1724&from=EN#d1e32-36-1) du règlement. Le périmètre des procédure est détaillé dans le document (en anglais) : [Explanatory document on scope of Annex II procedures_Sept 2020-1.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/b83ca995-de12-48f8-9338-307ac8593111/Explanatory_document_on_scope_of_Annex_II_procedures_Sept_2020-1.pdf)</p>
+      <p>Le Pilier II couvre aussi, dans le cadre des Procédures de l'Annexe II, l'OOP (Once Only Principle), relatif à l'authentification et au [Dites-le nous une fois](https://eur-lex.europa.eu/legal-content/FR/TXT/HTML/?uri=CELEX:32018R1724&from=EN#d1e1761-1-1). Il s'agit d'un sujet très technique dont l'Acte d'exécution est encore en cours de discussion; par ailleurs, il est porté par d'autres acteurs. Par conséquent, il ne sera pas abordé en tant que tel ici.</p>
+
+      <h3>Comment nous l'appliquons</h3>
+      <p>Par souci d'optimisation des ressources (limitées) et de cohérence avec nos autres missions, la décision a été prise de suivre les démarches françaises concernées via l'[Observatoire de la qualité des démarches en ligne](https://observatoire.numerique.gouv.fr/).<br>
+      Cette approche suppose :</p>
+      <ul>
+        <li>d'établir un lien entre les démarches de l'Observatoire et les Procédures du PNU, sachant qu'une Procédure PNU peut ne pas exister en France, ou bien que plusieurs démarches de l'Observatoire puissent concerner une même Procédure PNU.</li>
+        <li>d'identifier les démarches concernées mais qui ne sont pas suivies dans le cadre de l'Observatoire et prendre une décision les concernant (par exemple, les intégrer dans l'Observatoire).</li>
+      </ul>  
+      <p>Par exemple, la [démarche](https://www.cleiss.fr/docs/textes/883-04/t2.html) concernant la Procédure PNU "Demander à établir quelle est la législation applicable conformément au titre II du règlement (CE) no 883/2004 du Parlement européen et du Conseil" (détermination de la Législation Sociale Applicable) n'est pas suivi dans l'Observatoire (et n'est [pas dématérialisée](https://www.cleiss.fr/pdf/form_dla.pdf)).</p>
+      <p>Ce travail ne peut être effectué que par chacun des Ministères.</p>
+      <p>Le lien entre Procédures PNU et Démarches Observatoire a été fait sur AirTable (et demande à être complétée et validée par les Ministères; ils peuvent accéder à une [vue "Référent" qui permet le contrôle](https://airtable.com/shrHA6vtsvmvEJygE/tblkvBZs4Hwj5JDhF)). Le suivi par Démarche concernée fait l'objet d'une vue dédiée ("[Vue PNU](https://airtable.com/appQEhagAVjFbQgUy/tblZBbUVBRh33yy7g/viwAmfeLrCSAo4tbl?blocks=hide)") dans AirTable.</p>
+
+      <h2>Les exigences du PNU (pilier II)</h2>
+      <h3>Ce qui est déjà suivi dans le cadre de l'Observatoire</h3>
+      <h4>Dématérialisation</h4>
+    
+    <p>Chaque État membre veille à ce que les utilisateurs puissent accéder à toutes les procédures énumérées à l’annexe II et les accomplir intégralement en ligne.</p>
+    <p>L'Annexe II précise le "résultat de la démarche" attendu (par exemple "Accusé de réception de la demande"). Ce "résultat" doit être lui aussi dématérialisé.</p>
+    <p>Les procédures visées [...] sont considérées comme étant intégralement en ligne lorsque :</p>
+    <ul>
+      <li>a) l’identification des utilisateurs, la fourniture d’informations et de justificatifs, la signature et la validation définitive peuvent être effectuées par voie électronique à distance, par l’intermédiaire d’une chaîne de services qui permet aux utilisateurs de respecter de façon simple et structurée les exigences de la procédure;</li>
+      <li>b) les utilisateurs reçoivent un accusé de réception automatique, à moins que le résultat de la procédure soit communiqué immédiatement;</li>
+      <li>c) le résultat de la procédure est communiqué par voie électronique ou, lorsque c’est nécessaire pour respecter le droit applicable de l’Union ou le droit national, communiqué par des moyens physiques;</li>
+      <li>d) les utilisateurs reçoivent une notification électronique d’achèvement de la procédure.</li>
+    </ul>
+    <p>En d'autres termes :</p>
+    <p lang="en">In many procedures only the first steps are digitalised: users can fill in an application form online and submit it to the relevant authorities. However, the output of the procedure is still delivered only by physical means. Sometimes, the national users can receive the output in an electronic form provided that they are registered and are using the specific delivery system. Such system is however not available for cross-border users. [...] the cross-border users must be able to receive the output of the procedure in electronic format in all cases where this is also possible for non-cross border users. The electronic delivery can be followed by a physical delivery if the national or Union law so requires. Cf. Guideline on cross-border access to online procedure p. 20 : [Guidelines on Cross-Border Access to online procedures_September 2020-1.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/c04b7581-d19e-4c93-81e7-29b692c09d36/Guidelines_on_Cross-Border_Access_to_online_procedures_September_2020-1.pdf)</p>
+    <h4>Recueil des avis utilisateurs</h4>
+    <p>Afin de recueillir directement des informations émanant des utilisateurs sur leur degré de satisfaction quant aux services fournis par l’intermédiaire du portail et aux informations proposées par celui-ci, la Commission met à la disposition des utilisateurs, sur le portail, un outil facile d’emploi leur permettant de donner leur avis et de formuler anonymement des commentaires sur la qualité et la disponibilité des services [...]. Les autorités compétentes n’ont pas l’obligation [...] de donner aux utilisateurs l’accès à l’outil de recueil d’avis des utilisateurs  [...] lorsqu’un autre outil de recueil d’avis d’utilisateurs présentant des fonctionnalités analogues  [...] est déjà proposé  [...]. Les autorités compétentes recueillent les avis des utilisateurs reçus par l’intermédiaire de leur propre outil de recueil d’avis des utilisateurs et les font suivre à la Commission et aux coordonnateurs nationaux des autres États membres.</p>
+    <p>Le suivi dans le cadre de l'Observatoire impose l'utilisation du bouton et du formulaire "Je donne mon avis" (JDMA). Il conviendra donc de mener un chantier pour remonter les données recueillis au format et à la fréquence attendue à la Commission. Un traduction en anglais du formulaire (et du bouton?) est également à prévoir.</p>
+    <p>Documentation</p>
+    <ul>
+        <li>[CELEX 32020R1121 FR TXT - Imp. Act Stats and feedback.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/27afe172-19f4-4747-812a-d310a14333e5/CELEX_32020R1121_FR_TXT_-_Imp._Act_Stats_and_feedback.pdf)</li>
+        <li>[https://webgate.ec.europa.eu/fpfis/wikis/pages/viewpage.action?pageId=421888704](https://webgate.ec.europa.eu/fpfis/wikis/pages/viewpage.action?pageId=421888704)</li>
+    </ul>    
+    <h4>Accessibilité</h4>
+    <p>Il convient que la Commission et les États membres veillent au respect de la convention des Nations unies relative aux droits des personnes handicapées, et en particulier ses articles 9 et 21, et il convient, pour favoriser l’accès aux informations des personnes présentant des déficiences intellectuelles, de fournir, dans la plus large mesure possible et conformément au principe de proportionnalité, des versions dans une langue facile à comprendre. Les États membres en procédant à la ratification et l’Union en procédant à la conclusion de la convention des Nations unies relative aux droits des personnes handicapées se sont engagés à prendre des mesures appropriées pour assurer aux personnes handicapées, sur la base de l’égalité avec les autres, l’accès aux nouveaux systèmes et aux nouvelles technologies de l’information et de la communication, y compris l’internet, en facilitant l’accès aux informations des personnes présentant des déficiences intellectuelles grâce à la fourniture, dans la plus large mesure possible et de manière proportionnée, de versions dans une langue facile à comprendre. </p>
+
+    <h3>Ce qu'il faut suivre par ailleurs</h3>
+    <h4>Informations sur la démarche</h4>
+    <p>Les informations doivent être traduites en anglais.</p>
+    <p>Les utilisateurs ont la possibilité de consulter les instructions leur permettant d’accomplir la procédure dans une langue officielle de l’Union largement comprise par le plus grand nombre possible d’utilisateurs transfrontières.</p>
+    <p>En effet, les informations nécessaires à l'accomplissement de la démarche doivent être disponibles en anglais, ce qui dispense à la démarche elle-même de l'être : <span lang="en">The SDGR requires that the explanations on how to fill in the procedure form are provided in English. The SDGR does not require that the procedure can be carried out in English. It is not expected that users provide their input in English nor that the decision will be issued in English.</span></p>
+    <p>La Commission prend en charge la traduction d'un certain nombre de pages sur demande. Cf. Guideline on cross-border access to online procedure p. 10 : [Guidelines on Cross-Border Access to online procedures_September 2020-1.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/46764638-f763-4e15-b537-a1cfb6560883/Guidelines_on_Cross-Border_Access_to_online_procedures_September_2020-1.pdf)</p>
+     
+    <h4>Authentification</h4>
+    <p>Dans tous les cas où cela est possible également pour les utilisateurs non transfrontières, les utilisateurs transfrontières sont en mesure de s’identifier et de s’authentifier, de signer ou de sceller des documents électroniquement.</p>
+    <p>Ce sujet est complexe, d'autant plus qu'il est en lien avec Once Only Principle dont l'acte d’exécution est en cours d'élaboration. En pratique, il faudra proposer :</p>
+    <ul>
+      <li>FranceConnect+ qui devra être capable de reconnaître les identités numériques des différents pays ;</li>
+      <li>ProConnect pour les personnes morales ;</li>
+      <li>Ou prévoir une alternative pour les utilisateurs transfrontaliers.</li>
+    </ul>
+    <p>En amont, il convient de s'assurer que FranceConnect+ et ProConnect sont bien opérationnels et répondent aux exigences du PNU. À voir notamment avec Christine Balian à la Dinum.</p>
+    <p>Documents et échanges sur le sujet :</p>
+    <ul>        
+      <li>https://digital-strategy.ec.europa.eu/en/policies/eidas-regulation](https://digital-strategy.ec.europa.eu/en/policies/eidas-regulation)</li>  
+      <li>[210624 FC FCplus échange avec Christine.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/4ee01555-7bef-4d93-bdb3-f55bffde6f73/210624_FC_FCplus_change_avec_Christine.pdf)</li>
+      <li>[210603 eIDAS - Communiqué de presse.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/365d9a7d-d9dc-4a16-9364-99dade3ecf68/210603_eIDAS_-_Communiqu_de_presse.pdf)</li>
+      <li>[210603 eIDAS - Questions-Réponses.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/0a260c51-d10d-4f58-9b50-9422e9f1cf49/210603_eIDAS_-_Questions-Rponses.pdf)</li>
+      <li>[210708 PNU - authentification entreprises mail.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/d597e56e-d2a2-4d49-986a-be2dc7fc2936/210708_PNU_-_authentification_entreprises_mail.pdf)</li>
+      </ul>
+    
+    <h4>Champs à saisir</h4>
+    <p>Les utilisateurs transfrontières ont la possibilité de soumettre les informations requises, y compris lorsque leur structure diffère de celle d’informations analogues dans l’État membre concerné.</p>
+    <p>Les démarches en ligne doivent être pensées pour tous les utilisateurs européens: champs adapatés, alphabets européens acceptés (en pratique, les caractères spéciaux UTF8), pièces justificatives dématérialisées pour tous.</p>
+    <p>The structure of basic information required in administrative procedures such as an address, a postcode, a telephone number, a social security number, a national identification number varies from one Member State to another. The specific way in which such information is provided in a given Member State is often reflected in the formats the data is accepted in within the national procedures. [...] Across Europe, three different alphabets are used: Latin, Cyrillic and Greek and each has a different character set. Even within the Latin alphabet, additional characters or special signs are added.</p>
+    <p>The SDGR provides that where competent authorities are allowed, under applicable national law or administrative practices, to accept digitalised copies of non-electronic evidence of identity, such as identity cards or passports, those authorities shall also accept such digitalised copies from cross-border users. Cf. Guideline on cross-border access to online procedure p. 12 et p. 17 :[Guidelines on Cross-Border Access to online procedures_September 2020-1.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/2877d693-2739-4465-9414-5b3512c5e263/Guidelines_on_Cross-Border_Access_to_online_procedures_September_2020-1.pdf)</p>
+    
+    <h4>Paiement</h4>
+    <p>Si la procédure nécessite un paiement, celui-ci doit pouvoir être effectué en ligne quelque soit le pays européen de l'utilisateur. Il faut donc utiliser des solutions disponibles partout.</p>
+    <p>lorsqu’un paiement est exigé pour accomplir une procédure, les utilisateurs ont la possibilité d’acquitter tous les frais en ligne au moyen de services de paiement transfrontières largement disponibles, sans discrimination fondée sur le lieu d’établissement du prestataire de services de paiement, le lieu d’émission de l’instrument de paiement ou la localisation du compte de paiement dans l’Union. Cf. Guideline on cross-border access to online procedure p. 19 :[Guidelines on Cross-Border Access to online procedures_September 2020-1.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/867a229b-41db-4aa6-9100-ff8f64fa42ea/Guidelines_on_Cross-Border_Access_to_online_procedures_September_2020-1.pdf)</p>
+    
+    <h4>Statistiques</h4>
+    <p>Les autorités compétentes et la Commission veillent à ce que des statistiques soient collectées concernant les visites des utilisateurs sur le portail et les pages internet auxquelles le portail renvoie, d’une manière qui préserve l’anonymat des utilisateurs, dans le souci d’améliorer la fonctionnalité du portail. </p> 
+    <p>Les statistiques recueillies [...] font partie des catégories suivantes: a) données relatives au nombre, à l’origine et au type d’utilisateurs du portail; b) données relatives aux préférences et aux parcours des utilisateurs; c) données relatives à la facilité d’utilisation, à la repérabilité et à la qualité des informations, des procédures et des services d’assistance et de résolution de problèmes. </p>
+    <p>Les statistiques doivent être communiquées à la Commission, qui fournit des solutions techniques. D'autres choix peuvent être faits, charge alors aux États Membres de communiquer les résultats au bon format à la Commission.</p>    
+    <p>**DOCUMENTATION TECHNIQUE À INTÉGRER** : [https://webgate.ec.europa.eu/fpfis/wikis/pages/viewpage.action?pageId=421888704](https://webgate.ec.europa.eu/fpfis/wikis/pages/viewpage.action?pageId=421888704)</p>
+    
+    <h4>Logo</h4>
+    
+    <p>Article 22.2 of the SDGR requires the Member States to include the Your Europe logo on national portals as a marker of quality referring to a set of criteria in the regulation. The quality criteria ensure the information on rules, procedures, assistance and problem-solving services are held to a certain standard. It is with this in mind that the Your Europe logo should become a recognisable image for the user. The logo should be visible and readable and contain a click-through hyperlink to the Your Europe general homepage ([https://europa.eu/youreurope/index.htm#en](https://europa.eu/youreurope/index.htm#en)). [...] The logo will appear on all national and EU level pages with information on rules, rights and obligations, as well as on procedures and on assistance service pages that have been notified as part of the gateway. *[SDG - Links on National Webpages Guidelines]*</p>
+    <p>Documentation :</p>
+    <ul>
+    <li>[SDG - Links on National Webpages Guidelines.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/27df74dc-3466-4b79-8700-c695e21073ec/SDG_-_Links_on_National_Webpages_Guidelines.pdf)</li>
+    <li>[SDG - Visual Guidelines for the logo.pdf](https://s3-us-west-2.amazonaws.com/secure.notion-static.com/879f63c8-ee6f-462a-af0d-c1a47c8aba0f/SDG_-_Visual_Guidelines_for_the_logo.pdf)</li>
+    <li>[https://webgate.ec.europa.eu/fpfis/wikis/display/SDGCOORDGROUP/Logo](https://webgate.ec.europa.eu/fpfis/wikis/display/SDGCOORDGROUP/Logo)</li>
+    </ul>
+
+
+
+
+    </div>
+
+  </Layout>
+</template>
+
+
+<script>
+
+  import Accessibilite from "~/assets/images/accessibilite.svg"
+
+  export default {
+    components: {
+      Accessibilite,
+    },
+    metaInfo: {
+      title: "Portail numérique unique - Procédures",
+      meta: [{
+          name: 'description',
+          content: ""
+        },
+        {
+          property: 'og:title',
+          content: "Portail numérique unique - Procédures - DesignGouv"
+        },
+        {
+          property: 'og:description',
+          content: "Portail numérique unique - Procédures"
+        },
+        {
+          property: 'og:image',
+          content: "https://design.numerique.gouv.fr/designGouv.png"
+        }],
+      }
+    }
+
+</script>
+
+
+<style lang="scss">
+
+  @import "src/assets/scss/_vars.scss";
+
+
+</style>
